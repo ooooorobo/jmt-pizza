52f4375c5b5c540abd9c926363cc1a9cc30b175c
ooooorobo <orobos654@gmail.com>
2020-10-11 21:53:30 +0900

    feat: 로비 씬에서 유저 코인 표시, 씬 매니저 스크립트 이동

---
+++ Assets/Scripts/SceneManager/LobbyManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Data;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviour
+{
+    [Header("UI")]
+    public Text userCoin;
+    
+    private UserData userData;
+
+    void Start()
+    {
+        userData = 
+            (UserData) Assets.Scripts.Data.DataManager.GetDataFromJson<UserData>("userData") 
+            ?? new UserData();
+        userCoin.text = userData.coin.ToString();
+    }
+}
