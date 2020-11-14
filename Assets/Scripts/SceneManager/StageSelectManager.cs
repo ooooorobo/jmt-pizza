982978cbf155e4957502afa4e5882b48e3aed1c5
ooooorobo <orobos654@gmail.com>
2020-11-14 23:10:10 +0900

    ui: 스테이지 선택 버튼 적용

--- Assets/Scripts/SceneManager/StageSelectManager.cs
+++ Assets/Scripts/SceneManager/StageSelectManager.cs
@@ -23,6 +23,7 @@ public class StageSelectManager : MonoBehaviour
     private void InitStageButtons()
     {		
 	    bool isDone = true;
+	    Color defaultColor = Color.white;
 
 	    for (int i = 0; i < stageCount; i++)
 	    {
@@ -31,20 +32,28 @@ public class StageSelectManager : MonoBehaviour
 		    newBtn.SetParent(scrollView);
 			
 		    // 스테이지 선택 버튼 텍스트 지정
-		    Text btnText = newBtn.GetChild(0).GetComponent<Text>();
-		    btnText.text = "Stage " + (i + 1);
+		    Text btnText = newBtn.GetChild(1).GetComponent<Text>();
+		    Image[] groupStars = newBtn.GetChild(2).GetComponentsInChildren<Image>();
+		    
+		    btnText.text = (i + 1).ToString();
+		    if (i < 10)
+		    {
+			    btnText.text = "0" + btnText.text;
+		    }
 
 		    // 마지막으로 클리어한 스테이지의 직후 스테이지까지만 플레이 가능함
 			if (userStageData.stages[i].isCleared)
 			{
-				btnText.text += "\n클리어!";
-				
 				int x = i;
 				newBtn.GetComponent<Button>().onClick.AddListener(() => StageLoader.Instance().SetStageData(x));
+
+				foreach (Image star in groupStars)
+				{
+					star.color = defaultColor;
+				}
 			}
 		    else
 		    {
-			    btnText.text += "\n아직!!";
 			    if (isDone) isDone = false;
 			    else newBtn.GetComponent<Button>().interactable = false;
 		    }
